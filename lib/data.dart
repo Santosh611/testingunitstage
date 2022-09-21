f0971e08c701b4c21c1efe92e5776dbb51673e14
Santosh A M <33995817+Santosh611@users.noreply.github.com>
2022-09-21 15:45:41 +0530

    first commit

---
+++ lib/data.dart
@@ -0,0 +1,102 @@
+var sample = {
+  "status": true,
+  "message": "",
+  "response": [
+    {
+      "_id": "Testing",
+      "sno": 10,
+      "icon-url":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/803107testing.png",
+      "icon-url1":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/697357testing-light.png"
+    },
+    {
+      "_id": "Open Badges",
+      "sno": 11,
+      "icon-url":
+          "https://entrustient-01.s3.amazonaws.com/staging/eKYC/Recipient/6-4-2022/292598Group18653%403x.png",
+      "icon-url1":
+          "https://entrustient-01.s3.amazonaws.com/staging/eKYC/Recipient/6-4-2022/941224Group18651%403x.png"
+    },
+    {
+      "_id": "Motor",
+      "sno": 8,
+      "icon-url":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/394280motor.png",
+      "icon-url1":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/903052motor-light.png"
+    },
+    {
+      "_id": "Insurance",
+      "sno": 6,
+      "icon-url":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/588776insurance.png",
+      "icon-url1":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/680826insurance-light.png"
+    },
+    {
+      "_id": "Identity",
+      "sno": 7,
+      "icon-url":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/300649identity.png",
+      "icon-url1":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/216071identity-light.png"
+    },
+    {
+      "_id": "Government",
+      "sno": 4,
+      "icon-url":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/384439goverment.png",
+      "icon-url1":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/732627goverment-light.png"
+    },
+    {
+      "_id": "Employment",
+      "sno": 2,
+      "icon-url":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/235289employment.png",
+      "icon-url1":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/115574employment-light.png"
+    },
+    {
+      "_id": "Education",
+      "sno": 1,
+      "icon-url":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/416268education.png",
+      "icon-url1":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/775342education-light.png"
+    },
+    {
+      "_id": "Health",
+      "sno": 5,
+      "icon-url":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/942313health.png",
+      "icon-url1":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/418605health-light.png"
+    },
+    {
+      "_id": "Manufacturing",
+      "sno": 9,
+      "icon-url":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/837849manafacturing.png",
+      "icon-url1":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/611160manafacturing-light.png"
+    },
+    {
+      "_id": "Entrepreneurship",
+      "sno": 3,
+      "icon-url":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/413689entrenprenur.png",
+      "icon-url1":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/111919entreprenur-light.png"
+    },
+    {
+      "_id": "Travel",
+      "sno": 12,
+      "icon-url":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/196985travel-light.png",
+      "icon-url1":
+          "https://entrustient-01.s3.amazonaws.com/staging/credential/uploadeddocument/5132021/196985travel-light.png"
+    }
+  ]
+};
---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<Album> fetchAlbum(http.Client client) async {
+  final response = await client.post(Uri.parse(
+      'https://staging.entrustient.net/service/api/recipient/v3.1.1/credential/getCategory'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class Album {
+  bool? status;
+  String? message;
+  List<Response>? response;
+
+  Album({this.status, this.message, this.response});
+
+  Album.fromJson(Map<String, dynamic> json) {
+    status = json['status'];
+    message = json['message'];
+    if (json['response'] != null) {
+      response = <Response>[];
+      json['response'].forEach((v) {
+        response!.add(new Response.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['status'] = this.status;
+    data['message'] = this.message;
+    if (this.response != null) {
+      data['response'] = this.response!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Response {
+  String? sId;
+  int? sno;
+  String? iconUrl;
+  String? iconUrl1;
+
+  Response({this.sId, this.sno, this.iconUrl, this.iconUrl1});
+
+  Response.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    sno = json['sno'];
+    iconUrl = json['icon-url'];
+    iconUrl1 = json['icon-url1'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['_id'] = this.sId;
+    data['sno'] = this.sno;
+    data['icon-url'] = this.iconUrl;
+    data['icon-url1'] = this.iconUrl1;
+    return data;
+  }
+}
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum(http.Client());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<Album>(
+            future: futureAlbum,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.status.toString());
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
